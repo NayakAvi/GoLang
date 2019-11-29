8ff2e96e5fd76985b5301acbfd84a9cace864887
NayakAvi <avinashun77@gmail.com>
2019-11-29 10:24:14 +0530

    Add files via upload

--- 02. Variables/01. variable Declaration/SimpleVariables.go
+++ 02. Variables/01. variable Declaration/SimpleVariables.go
@@ -37,32 +37,49 @@ func main() {
 	user4Age := 10
 
 
+	//type5
+	var user5FirstName , user5SecondName ,user5Age = "user4FirstName" ,"user4SecondName" , 10
+
 	user1FirstName = "user1FirstName"
 	user1SecondName = "user1SecondName"
 	user1Age = 10
 
 	user2FirstName = "user2FirstName"
 	user2SecondName = "user2SecondName"
-	user1Age = 20
+	user2Age = 20
 
 	user3FirstName = "user3FirstName"
 	user3SecondName = "user3SecondName"
 	user3Age = 10
 
+	fmt.Println("************************************************")
+
 	fmt.Println(user1FirstName)
 	fmt.Println(user1SecondName)
 	fmt.Println("user1Age : ", user1Age)
+
+	fmt.Println("************************************************")
 
 	fmt.Println(user2FirstName)
 	fmt.Println(user2SecondName)
 	fmt.Println("user2Age : ", user2Age)
 
+	fmt.Println("************************************************")
+
 	fmt.Println(user3FirstName)
 	fmt.Println(user3SecondName)
 	fmt.Println("user3Age : ", user3Age)
 
+	fmt.Println("************************************************")
+
 	fmt.Println(user4FirstName)
 	fmt.Println(user4SecondName)
 	fmt.Println("user4Age : ", user4Age)
+
+	fmt.Println("************************************************")
+
+	fmt.Println(user5FirstName)
+	fmt.Println(user5SecondName)
+	fmt.Println("user4Age : ", user5Age)
 
 }
